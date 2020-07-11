106a6515c3dbd9b90a78b19682c5e23a2586c039
Julian Tallar <jtallar@itba.edu.ar>
2020-07-11 16:16:24 -0300

    Clickable logo

--- public/logo.js
+++ public/logo.js
@@ -8,7 +8,11 @@ Vue.component('logo', {
     template:
         `<v-container>
             <div class="d-flex justify-center">
-                <v-img :max-height="getIconSize" src="./img/logo.png" alt="logo" contain position="center"></v-img>
+                <v-btn icon href="./index.html" :height="getIconSize">
+                    <v-avatar :size="getIconSize">
+                        <v-img :max-height="getIconSize" src="./img/logo.png" alt="logo" contain position="center"></v-img>
+                    </v-avatar>
+                </v-btn>
             </div>
         </v-container>`,
     computed: {
