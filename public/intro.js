b5658cfdc8267251ca285f0e31da4d1bc54228b4
Julian Tallar <jtallar@itba.edu.ar>
2020-08-16 20:27:41 -0300

    Added prize to intro

--- public/intro.js
+++ public/intro.js
@@ -14,6 +14,17 @@ Vue.component('intro', {
             <b>¡CUIDADO!</b> Acordate la respuesta de la última pregunta que resolviste, la próxima vez que
             entres la va a necesitar para continuar donde dejaste.</br>
             <b>¿PISTAS?</b> Si te quedás trabado en alguna pregunta, mandá un DM a @computer.society.itba, estamos para servirte.</br>
+            <v-expansion-panels flat hover>
+            <v-expansion-panel>
+                <v-expansion-panel-header>¿Qué está en juego (además de tu honor)?</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    Las <b>primeras 10</b> personas que suban una historia con la captura de la pantalla ganadora 
+                    y nos envíen el texto secreto por privado ganarán <b>1 pista</b> para el Qué ves? Pero eso no es todo. 
+                    Si lográs llegar hasta el final antes del <b>Domingo 30/08 a las 23:59</b> y nos compartís 
+                    esa pantalla, ganás <b>1 chance</b> para un sorteo que haremos en breve por una Giftcard.</br>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+            </v-expansion-panels>
             ¡Suerte!
             </p>
             </br>
